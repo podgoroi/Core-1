b24f2099c384655dde0b6599f582e8b7bb114bba
Teperikov_Anton <lastsearching31@mail.ru>
2024-11-26 22:57:16 +0300

    Правки тестов

--- src/test/MethodsTest.java
+++ src/test/MethodsTest.java
@@ -1,212 +1,181 @@
 package test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
 import task.methods.Methods;
 
 public class MethodsTest {
 
-  Methods methods = new Methods();
+  public static void main(String[] args) {
+    Methods methods = new Methods();
 
-  @Test
-  public void testStringToLowerCase() {
-    //given
-    String expectedResult = "string";
+    // Тест 1: testStringToLowerCase
+    String expectedResult1 = "string";
+    String actualResult1 = methods.stringToLowerCase("STRING");
+    if (expectedResult1.equals(actualResult1)) {
+      System.out.println("Тест 1 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 1 не пройден. Ожидался: " + expectedResult1 + ", получен: " + actualResult1);
+    }
 
-    //when
-    String actualResult = methods.stringToLowerCase("STRING");
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testSumOfTwoNumbers() {
-    //given
+    // Тест 2: testSumOfTwoNumbers
     int a = 2;
     int b = 3;
-    int expectedResult = 5;
+    int expectedResult2 = 5;
+    int actualResult2 = methods.sumOfTwoNumbers(a, b);
+    if (expectedResult2 == actualResult2) {
+      System.out.println("Тест 2 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 2 не пройден. Ожидался: " + expectedResult2 + ", получен: " + actualResult2);
+    }
 
-    //when
-    int actualResult = methods.sumOfTwoNumbers(a, b);
+    // Тест 3: testSumOfTwoStrings
+    String str1 = "Hello";
+    String str2 = "World";
+    String expectedResult3 = "HelloWorld";
+    String actualResult3 = methods.sumOfTwoStrings(str1, str2);
+    if (expectedResult3.equals(actualResult3)) {
+      System.out.println("Тест 3 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 3 не пройден. Ожидался: " + expectedResult3 + ", получен: " + actualResult3);
+    }
 
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testSumOfTwoStrings() {
-    //given
-    String a = "Hello";
-    String b = "World";
-    String expectedResult = "HelloWorld";
-
-    //when
-    String actualResult = methods.sumOfTwoStrings(a, b);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testSumOfStringAndNumber() {
-    //given
+    // Тест 4: testSumOfStringAndNumber
     String str = "10";
     int num = 5;
-    String expectedResult = "15";
+    String expectedResult4 = "15";
+    String actualResult4 = methods.sumOfStringAndNumber(str, num);
+    if (expectedResult4.equals(actualResult4)) {
+      System.out.println("Тест 4 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 4 не пройден. Ожидался: " + expectedResult4 + ", получен: " + actualResult4);
+    }
 
-    //when
-    String actualResult = methods.sumOfStringAndNumber(str, num);
+    // Тест 5: testCompareStringNumberWithInt
+    String strNum = "5";
+    int numComp = 10;
+    boolean expectedResult5 = true;
+    boolean actualResult5 = methods.compareStringNumberWithInt(strNum, numComp);
+    if (expectedResult5 == actualResult5) {
+      System.out.println("Тест 5 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 5 не пройден. Ожидался: " + expectedResult5 + ", получен: " + actualResult5);
+    }
 
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
+    // Тест 6: testCheckBooleans
+    boolean aBool = true;
+    boolean bBool = false;
+    boolean expectedResult6 = true;
+    boolean actualResult6 = methods.checkBooleans(aBool, bBool);
+    if (expectedResult6 == actualResult6) {
+      System.out.println("Тест 6 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 6 не пройден. Ожидался: " + expectedResult6 + ", получен: " + actualResult6);
+    }
 
-  @Test
-  public void testCompareStringNumberWithInt() {
-    //given
-    String str = "5";
-    int num = 10;
-    boolean expectedResult = true;
+    // Тест 7: testIsEven
+    int numEven = 4;
+    boolean expectedResult7 = true;
+    boolean actualResult7 = methods.isEven(numEven);
+    if (expectedResult7 == actualResult7) {
+      System.out.println("Тест 7 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 7 не пройден. Ожидался: " + expectedResult7 + ", получен: " + actualResult7);
+    }
 
-    //when
-    boolean actualResult = methods.compareStringNumberWithInt(str, num);
+    // Тест 8: testCheckArrayEvenOdd
+    int[] arrEvenOdd = {2, 4, 6};
+    String expectedResult8 = "Массив четных чисел";
+    String actualResult8 = methods.checkArrayEvenOdd(arrEvenOdd);
+    if (expectedResult8.equals(actualResult8)) {
+      System.out.println("Тест 8 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 8 не пройден. Ожидался: " + expectedResult8 + ", получен: " + actualResult8);
+    }
 
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
+    // Тест 9: testSumArrayElements
+    int[] arrSumElements = {1, 2, 3};
+    int[] expectedResult9 = {1, 3, 6};
+    int[] actualResult9 = methods.sumArrayElements(arrSumElements);
+    if (java.util.Arrays.equals(expectedResult9, actualResult9)) {
+      System.out.println("Тест 9 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 9 не пройден. Ожидался: " + java.util.Arrays.toString(expectedResult9)
+              + ", получен: " + java.util.Arrays.toString(actualResult9));
+    }
 
-  @Test
-  public void testCheckBooleans() {
-    //given
-    boolean a = true;
-    boolean b = false;
-    boolean expectedResult = true;
+    // Тест 10: testSumArray
+    int[] arrSum = {1, 2, 3};
+    int expectedResult10 = 6;
+    int actualResult10 = methods.sumArray(arrSum);
+    if (expectedResult10 == actualResult10) {
+      System.out.println("Тест 10 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 10 не пройден. Ожидался: " + expectedResult10 + ", получен: " + actualResult10);
+    }
 
-    //when
-    boolean actualResult = methods.checkBooleans(a, b);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testIsEven() {
-    //given
-    int num = 4;
-    boolean expectedResult = true;
-
-    //when
-    boolean actualResult = methods.isEven(num);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testCheckArrayEvenOdd() {
-    //given
-    int[] arr = {2, 4, 6};
-    String expectedResult = "Массив четных чисел";
-
-    //when
-    String actualResult = methods.checkArrayEvenOdd(arr);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testSumArrayElements() {
-    //given
-    int[] arr = {1, 2, 3};
-    int[] expectedResult = {1, 3, 6};
-
-    //when
-    int[] actualResult = methods.sumArrayElements(arr);
-
-    //then
-    assertArrayEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testSumArray() {
-    //given
-    int[] arr = {1, 2, 3};
-    int expectedResult = 6;
-
-    //when
-    int actualResult = methods.sumArray(arr);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testAgeCalculations() {
-    //given
+    // Тест 11: testAgeCalculations
     int years = 10;
-    int expectedResult = 120;
+    int expectedResult11 = 120;
+    int actualResult11 = methods.ageInMonths(years);
+    if (expectedResult11 == actualResult11) {
+      System.out.println("Тест 11 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 11 не пройден. Ожидался: " + expectedResult11 + ", получен: " + actualResult11);
+    }
 
-    //when
-    int actualResult = methods.ageInMonths(years);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testAreNamesEqual() {
-    //given
+    // Тест 12: testAreNamesEqual
     String name1 = "John";
     String name2 = "john";
-    boolean expectedResult = true;
+    boolean expectedResult12 = true;
+    boolean actualResult12 = methods.areNamesEqual(name1, name2);
+    if (expectedResult12 == actualResult12) {
+      System.out.println("Тест 12 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 12 не пройден. Ожидался: " + expectedResult12 + ", получен: " + actualResult12);
+    }
 
-    //when
-    boolean actualResult = methods.areNamesEqual(name1, name2);
+    // Тест 13: testGetLastElement
+    int[] arrLastElement = {1, 2, 3};
+    int expectedResult13 = 3;
+    int actualResult13 = methods.getLastElement(arrLastElement);
+    if (expectedResult13 == actualResult13) {
+      System.out.println("Тест 13 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 13 не пройден. Ожидался: " + expectedResult13 + ", получен: " + actualResult13);
+    }
 
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
+    // Тест 14: testCountLetterA
+    String strCountA = "AaA";
+    int expectedResult14 = 3;
+    int actualResult14 = methods.countLetterA(strCountA);
+    if (expectedResult14 == actualResult14) {
+      System.out.println("Тест 14 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 14 не пройден. Ожидался: " + expectedResult14 + ", получен: " + actualResult14);
+    }
 
-  @Test
-  public void testGetLastElement() {
-    //given
-    int[] arr = {1, 2, 3};
-    int expectedResult = 3;
-
-    //when
-    int actualResult = methods.getLastElement(arr);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testCountLetterA() {
-    //given
-    String str = "AaA";
-    int expectedResult = 3;
-
-    //when
-    int actualResult = methods.countLetterA(str);
-
-    //then
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void testWallet() {
-    //given
+    // Тест 15: testWallet
     int amount = 100;
-    String expectedResult = "Остаток в кошельке: 900";
-
-    //when
-    String actualResult = methods.withdraw(amount);
-
-    //then
-    assertEquals(expectedResult, actualResult);
+    String expectedResult15 = "Остаток в кошельке: 900";
+    String actualResult15 = methods.withdraw(amount);
+    if (expectedResult15.equals(actualResult15)) {
+      System.out.println("Тест 15 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест 15 не пройден. Ожидался: " + expectedResult15 + ", получен: " + actualResult15);
+    }
   }
 }
--- src/test/OopTest.java
+++ src/test/OopTest.java
@@ -1,68 +1,147 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
 import task.oop.Child1;
 import task.oop.Child2;
 import task.oop.Parent;
 
 public class OopTest {
 
-  @Test
-  public void testParentMethods() {
+  public static void main(String[] args) {
+    // Тестирование методов родительского класса
     Parent parent = new Parent();
 
     // Test sumOfTwoNumbers
-    assertEquals(5, parent.sumOfTwoNumbers(2, 3));
+    int expectedSumParent = 5;
+    int actualSumParent = parent.sumOfTwoNumbers(2, 3);
+    if (expectedSumParent == actualSumParent) {
+      System.out.println("Тест sumOfTwoNumbers родителя пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест sumOfTwoNumbers родителя не пройден. Ожидался: " + expectedSumParent + ", получен: "
+              + actualSumParent);
+    }
 
     // Test sumOfStringAndNumber
-    assertEquals(15, parent.sumOfStringAndNumber("10", 5));
+    int expectedSumStringAndNumberParent = 15;
+    int actualSumStringAndNumberParent = parent.sumOfStringAndNumber("10", 5);
+    if (expectedSumStringAndNumberParent == actualSumStringAndNumberParent) {
+      System.out.println("Тест sumOfStringAndNumber родителя пройден успешно!");
+    } else {
+      System.out.println("Тест sumOfStringAndNumber родителя не пройден. Ожидался: "
+          + expectedSumStringAndNumberParent + ", получен: " + actualSumStringAndNumberParent);
+    }
 
     // Test returnString
-    assertEquals("Hello", parent.returnString("Hello"));
-
-    // Test printParent (cannot test void methods directly)
+    String expectedReturnStringParent = "Hello";
+    String actualReturnStringParent = parent.returnString("Hello");
+    if (expectedReturnStringParent.equals(actualReturnStringParent)) {
+      System.out.println("Тест returnString родителя пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест returnString родителя не пройден. Ожидался: " + expectedReturnStringParent
+              + ", получен: " + actualReturnStringParent);
+    }
 
     // Test concatenateStrings
-    assertEquals("HelloWorld", parent.concatenateStrings("Hello", "World"));
-  }
+    String expectedConcatenateStringsParent = "HelloWorld";
+    String actualConcatenateStringsParent = parent.concatenateStrings("Hello", "World");
+    if (expectedConcatenateStringsParent.equals(actualConcatenateStringsParent)) {
+      System.out.println("Тест concatenateStrings родителя пройден успешно!");
+    } else {
+      System.out.println("Тест concatenateStrings родителя не пройден. Ожидался: "
+          + expectedConcatenateStringsParent + ", получен: " + actualConcatenateStringsParent);
+    }
 
-  @Test
-  public void testChild1Methods() {
+    // Тестирование методов Child1
     Child1 child1 = new Child1();
 
     // Test sumOfTwoNumbers
-    assertEquals(6, child1.sumOfTwoNumbers(2, 3));
+    int expectedSumChild1 = 6;
+    int actualSumChild1 = child1.sumOfTwoNumbers(2, 3);
+    if (expectedSumChild1 == actualSumChild1) {
+      System.out.println("Тест sumOfTwoNumbers Child1 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест sumOfTwoNumbers Child1 не пройден. Ожидался: " + expectedSumChild1 + ", получен: "
+              + actualSumChild1);
+    }
 
     // Test sumOfStringAndNumber
-    assertEquals(30, child1.sumOfStringAndNumber("10", 5));
+    int expectedSumStringAndNumberChild1 = 30;
+    int actualSumStringAndNumberChild1 = child1.sumOfStringAndNumber("10", 5);
+    if (expectedSumStringAndNumberChild1 == actualSumStringAndNumberChild1) {
+      System.out.println("Тест sumOfStringAndNumber Child1 пройден успешно!");
+    } else {
+      System.out.println("Тест sumOfStringAndNumber Child1 не пройден. Ожидался: "
+          + expectedSumStringAndNumberChild1 + ", получен: " + actualSumStringAndNumberChild1);
+    }
 
     // Test returnString
-    assertEquals("H", child1.returnString("Hello"));
-
-    // Test printParent (cannot test void methods directly)
+    String expectedReturnStringChild1 = "H";
+    String actualReturnStringChild1 = child1.returnString("Hello");
+    if (expectedReturnStringChild1.equals(actualReturnStringChild1)) {
+      System.out.println("Тест returnString Child1 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест returnString Child1 не пройден. Ожидался: " + expectedReturnStringChild1
+              + ", получен: " + actualReturnStringChild1);
+    }
 
     // Test concatenateStrings
-    assertEquals("Hello", child1.concatenateStrings("Hello", "World"));
-  }
+    String expectedConcatenateStringsChild1 = "Hello";
+    String actualConcatenateStringsChild1 = child1.concatenateStrings("Hello", "World");
+    if (expectedConcatenateStringsChild1.equals(actualConcatenateStringsChild1)) {
+      System.out.println("Тест concatenateStrings Child1 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест concatenateStrings Child1 не пройден. Ожидался: " + expectedConcatenateStringsChild1
+              + ", получен: " + actualConcatenateStringsChild1);
+    }
 
-  @Test
-  public void testChild2Methods() {
+    // Тестирование методов Child2
     Child2 child2 = new Child2();
 
     // Test sumOfTwoNumbers
-    assertEquals(6, child2.sumOfTwoNumbers(1, 2, 3));
+    int expectedSumChild2 = 6;
+    int actualSumChild2 = child2.sumOfTwoNumbers(1, 2, 3);
+    if (expectedSumChild2 == actualSumChild2) {
+      System.out.println("Тест sumOfTwoNumbers Child2 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест sumOfTwoNumbers Child2 не пройден. Ожидался: " + expectedSumChild2 + ", получен: "
+              + actualSumChild2);
+    }
 
     // Test sumOfStringAndNumber
-    assertEquals(6, child2.sumOfStringAndNumber("1", "2", "3"));
+    int expectedSumStringAndNumberChild2 = 6;
+    int actualSumStringAndNumberChild2 = child2.sumOfStringAndNumber("1", "2", "3");
+    if (expectedSumStringAndNumberChild2 == actualSumStringAndNumberChild2) {
+      System.out.println("Тест sumOfStringAndNumber Child2 пройден успешно!");
+    } else {
+      System.out.println("Тест sumOfStringAndNumber Child2 не пройден. Ожидался: "
+          + expectedSumStringAndNumberChild2 + ", получен: " + actualSumStringAndNumberChild2);
+    }
 
     // Test returnString
-    assertEquals("HelloWorld", child2.returnString("Hello", "World"));
-
-    // Test printParent (cannot test void methods directly)
+    String expectedReturnStringChild2 = "HelloWorld";
+    String actualReturnStringChild2 = child2.returnString("Hello", "World");
+    if (expectedReturnStringChild2.equals(actualReturnStringChild2)) {
+      System.out.println("Тест returnString Child2 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест returnString Child2 не пройден. Ожидался: " + expectedReturnStringChild2
+              + ", получен: " + actualReturnStringChild2);
+    }
 
     // Test concatenateStrings
-    assertEquals("HelloWorldJava", child2.concatenateStrings("Hello", "World", "Java"));
+    String expectedConcatenateStringsChild2 = "HelloWorldJava";
+    String actualConcatenateStringsChild2 = child2.concatenateStrings("Hello", "World", "Java");
+    if (expectedConcatenateStringsChild2.equals(actualConcatenateStringsChild2)) {
+      System.out.println("Тест concatenateStrings Child2 пройден успешно!");
+    } else {
+      System.out.println(
+          "Тест concatenateStrings Child2 не пройден. Ожидался: " + expectedConcatenateStringsChild2
+              + ", получен: " + actualConcatenateStringsChild2);
+    }
   }
 }
